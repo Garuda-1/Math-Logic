2318702cf893403bbacee99879bc9c7354f8ba6e
Ian <yan.dolganskiy@mail.ru>
2020-04-18 03:52:03 +0300

    task2 passed

--- task2/utils.hs
+++ task2/utils.hs
@@ -85,95 +85,92 @@ instance Eq Node where
 type DependencyMap = Map.Map Exp (Int, Int)
 type ReorderMap = Map.Map Int Int
 
-data Pack = Pack { getPrev :: Map.Map Exp Int, getMapImpl :: Map.Map Exp Node, getMapMP :: DependencyMap}
+data Pack = Pack { 
+  getPrev :: Map.Map Exp Int, 
+  getMapImpl :: Map.Map Exp (Set.Set Node),
+  getMapMP :: DependencyMap}
+  
+empty :: Pack
+empty = Pack Map.empty Map.empty Map.empty
+
+isLeaf :: Node -> [Exp] -> Bool
+isLeaf (Node _ exp) assumptions
+  = isAx1 exp 
+  || isAx2 exp
+  || isAx3 exp
+  || isAx4 exp
+  || isAx5 exp 
+  || isAx6 exp
+  || isAx7 exp
+  || isAx8 exp
+  || isAx9 exp
+  || isAx10 exp
+  || elem exp assumptions
 
 buildDependencies :: [Node] -> [Exp] -> Maybe DependencyMap
 buildDependencies [] _ = Just Map.empty
-buildDependencies exps assumptions =
-  let
-    isLeaf :: Node -> [Exp] -> Bool
-    isLeaf (Node _ exp) assumptions
-      = isAx1 exp 
-      || isAx2 exp
-      || isAx3 exp
-      || isAx4 exp
-      || isAx5 exp 
-      || isAx6 exp
-      || isAx7 exp
-      || isAx8 exp
-      || isAx9 exp
-      || isAx10 exp
-      || elem exp assumptions
-    
-    empty :: Pack
-    empty = (Pack Map.empty Map.empty Map.empty)
-    
-    insertIfAbsent :: Ord k => k -> v -> Map.Map k v -> Map.Map k v
+buildDependencies nodes assumptions =
+  let 
+    insertIfAbsent :: (Ord k) => k -> v -> Map.Map k v -> Map.Map k v
     insertIfAbsent key value map =
       case (Map.lookup key map) of
-        Just _ -> map
         Nothing -> Map.insert key value map
-      
-    nextPack :: Node -> Pack -> Pack
-    nextPack (Node index exp) pack =
+        _ -> map
+
+    nextPackImpl :: Node -> Pack -> Pack
+    nextPackImpl (Node index exp) (Pack prev mapImpl mapMP) =
       let
-        prev = getPrev pack
-        mapImpl = getMapImpl pack
-        mapMP = getMapMP pack
+        newPrev = insertIfAbsent exp index prev
       in
         case exp of
-          (EImpl a b) -> 
-            case (Map.lookup a prev) of
-              Just pindex ->
-                (Pack (insertIfAbsent exp index prev) (insertIfAbsent a (Node index exp) mapImpl) (insertIfAbsent b (pindex, index) mapMP))
-              Nothing -> 
-                (Pack (insertIfAbsent exp index prev) (insertIfAbsent a (Node index exp) mapImpl) mapMP)
-          _ ->
-            case (Map.lookup exp mapImpl) of
-              Just (Node pindex (EImpl _ nexp)) ->
-                (Pack (insertIfAbsent exp index prev) mapImpl (insertIfAbsent nexp (index, pindex) mapMP))
-              Nothing ->
-                (Pack (insertIfAbsent exp index prev) mapImpl mapMP)
+          (EImpl a b) ->
+            let
+              newMapImpl = case (Map.lookup a mapImpl) of
+                Just set -> Map.insert a (Set.insert (Node index exp) set) mapImpl
+                Nothing -> Map.insert a (Set.singleton (Node index exp)) mapImpl
+            in
+              case (Map.lookup a prev) of
+                Just pindex -> (Pack 
+                  newPrev 
+                  newMapImpl 
+                  (insertIfAbsent b (index, pindex) mapMP))
+                Nothing -> (Pack newPrev newMapImpl mapMP)
+          _ -> (Pack newPrev mapImpl mapMP)
           
-          
-          
-          
-          
-          
+    nextPackExp :: Node -> Pack -> Pack
+    nextPackExp (Node index exp) (Pack prev mapImpl mapMP) =
+      case (Map.lookup exp mapImpl) of
+        Just set ->
+          let
+            newMapMP = Set.foldr' 
+              (\(Node pindex (EImpl exp texp)) s -> insertIfAbsent texp (pindex, index) s)
+              mapMP set
+          in
+            Pack prev mapImpl newMapMP
+        _ -> Pack prev mapImpl mapMP
+        
+    nextPack :: Node -> Pack -> Pack
+    nextPack node pack = nextPackExp node $ nextPackImpl node pack          
     
     helper :: [Node] -> [Exp] -> Pack -> Maybe DependencyMap
     helper [] _ pack = Just (getMapMP pack)
-    helper ((Node index exp) : nodes) assumptions pack =
+    helper ((Node index exp) : nodes) assumptions pack = 
       let
-        nPack = (nextPack (Node index exp) pack)
+        newPack = nextPack (Node index exp) pack
       in
         if (isLeaf (Node index exp) assumptions) then
-          helper nodes assumptions nPack
+          helper nodes assumptions newPack
         else
-          case (Map.lookup exp (getMapMP nPack)) of
-            Just _ -> helper nodes assumptions nPack
+          case (Map.lookup exp (getMapMP newPack)) of
             Nothing -> Nothing
-        
-{--
-    lookupMP :: Node -> [Node] -> [(Node, Node)]
-    lookupMP (Node index exp) l = [(p0, p1) | 
-      p0 <- reverse l,
-      p1 <- reverse l,
-      getIndex p0 < index, 
-      getIndex p1 < index,
-      isMP (getExp p0) (getExp p1) exp]
+            Just (i1, i2) -> 
+              if ((max i1 i2) < index) then 
+                helper nodes assumptions newPack
+              else
+                Nothing
       
-    addDependencies :: [(Node, Node)] -> Maybe DependencyMap -> Int -> Maybe DependencyMap
-    addDependencies _ Nothing _ = Nothing
-    addDependencies mps (Just deps) i =
-      --Just $ foldr (\((Node i1 _), (Node i2 _)) s -> ((i1, i2), i) : s) deps mps
-      let
-        ((Node i1 _), (Node i2 _)) = head mps
-      in
-        Just (Map.insert i (i2, i1) deps)
-        --}
   in
-    helper exps assumptions empty
+    helper nodes assumptions empty
   
         
 
@@ -224,16 +221,21 @@ readLinesList = do
 
 
 
-pickNecessary :: Int -> [Node] -> DependencyMap -> [Node]
-pickNecessary _ [] _ = []
-pickNecessary index ((Node i exp) : nodes) deps =
+pickNecessary :: Int -> [Node] -> [Exp] -> DependencyMap -> [Node]
+pickNecessary _ [] _ _ = error "Little less but still epic"
+pickNecessary index ((Node i exp) : nodes) assumptions deps =
   if (index == i) then
-    case (Map.lookup exp deps) of
-      Nothing -> [(Node i exp)]
-      Just (i1, i2) -> 
-        (Node i exp) : ((pickNecessary i1 nodes deps) ++ (pickNecessary i2 nodes deps))
+    if (isLeaf (Node i exp) assumptions) then
+      [(Node i exp)]
+    else
+      case (Map.lookup exp deps) of
+        Nothing -> [(Node i exp)]
+        Just (i1, i2) -> 
+          (Node i exp) : 
+            ((pickNecessary i1 nodes assumptions deps) ++ 
+            (pickNecessary i2 nodes assumptions deps))
   else
-    pickNecessary index nodes deps
+    pickNecessary index nodes assumptions deps
     
     
 
@@ -272,6 +274,7 @@ toAnnotatedProof ((Node index exp) : nodes) assumptions deps old2new =
     assumptionString i = "[" ++ (show newIndex) ++ ". Hypothesis " ++ (show i)
       ++ "] " ++ (show exp)
       
+    
     helper :: String
     helper =
       if (isAx1 exp) then
@@ -297,50 +300,13 @@ toAnnotatedProof ((Node index exp) : nodes) assumptions deps old2new =
       else
         case (elemIndex exp assumptions) of
           Just i -> assumptionString (i + 1)
-          Nothing -> 
-            case (Map.lookup exp deps) of
-              Just (i1, i2) -> case (Map.lookup i1 old2new) of
-                Just ni1 -> case (Map.lookup i2 old2new) of
-                  Just ni2 -> mpString ni2 ni1
-                  Nothing -> error "M.P. dependency index not found"
-                Nothing -> error "M.P. dependency index not found"   
-              Nothing -> error "Epic fail"
-      
-    {--
-    helper :: String
-    helper =
-      case (Map.lookup exp deps) of
-        Just (i1, i2) -> case (Map.lookup i1 old2new) of
-          Just ni1 -> case (Map.lookup i2 old2new) of
-            Just ni2 -> mpString ni1 ni2
-            Nothing -> error "M.P. dependency index not found"
-          Nothing -> error "M.P. dependency index not found"   
-        Nothing ->
-          if (isAx1 exp) then
-            axString 1
-          else if (isAx2 exp) then
-            axString 2
-          else if (isAx3 exp) then
-            axString 3
-          else if (isAx4 exp) then
-            axString 4
-          else if (isAx5 exp) then
-            axString 5
-          else if (isAx6 exp) then
-            axString 6
-          else if (isAx7 exp) then
-            axString 7
-          else if (isAx8 exp) then
-            axString 8
-          else if (isAx9 exp) then
-            axString 9
-          else if (isAx10 exp) then
-            axString 10
-          else
-            case (elemIndex exp assumptions) of
-              Just i -> assumptionString (i + 1)
-              Nothing -> error "Assumption not found"
-              --}
+          Nothing -> case (Map.lookup exp deps) of
+            Just (i1, i2) -> case (Map.lookup i1 old2new) of
+              Just ni1 -> case (Map.lookup i2 old2new) of
+                Just ni2 -> mpString ni1 ni2
+                Nothing -> error "M.P. dependency index not found"
+              Nothing -> error "M.P. dependency index not found"   
+            Nothing -> error "Epic fail"
   in
     helper : (toAnnotatedProof nodes assumptions deps old2new)
     
@@ -361,7 +327,7 @@ main = do
       if ((getExp $ last nodes) /= (snd header)) then
         putStrLn "Proof is incorrect"
       else do
-        let minimized = nub $ sortOn (getIndex) $ pickNecessary (length nodes) (reverse nodes) deps
+        let minimized = nub $ sortOn (getIndex) $ pickNecessary (length nodes) (reverse nodes) assumptions deps
         let old2new = reorderingMaps minimized
         putStr $ intercalate ", " (map (show) assumptions)
         if (null assumptions) then
